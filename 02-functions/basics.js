3ae6c387ac4a02e77baafdfa61a16fdfd24acba8
xunne899 <yixun89@live.com>
2022-01-26 08:14:43 +0000

    add

---
+++ 02-functions/basics.js
@@ -0,0 +1,19 @@
+// function caldistance(speed,time){
+//     let distance = speed * time 
+//     return distance;
+//    }
+   
+   
+//    function calculateFuelRequired(distance) {
+//        return distance *0.5;
+//    }
+   
+//    let distance = caldistance (10,20)
+//    console.log("Distance travelled = ", distance);
+//    console.log ("fuel needed =", calculateFuelRequired(distance));
+ 
+   function multi (num) {
+    let total = num *2
+ return total;
+   }
+   console.log (multi (4))
