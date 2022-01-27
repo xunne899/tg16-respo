c84412d7ae4a4d38a2c2d0846e20a644652155cc
xunne899 <yixun89@live.com>
2022-01-27 07:35:10 +0000

    add

---
+++ 04-loops/loops.js
@@ -0,0 +1,43 @@
+
+/*
+while (an expression){
+    // code that i want to repeat
+}
+*/
+
+// print 10 gong xi fa cai
+
+// sentinel variables
+
+// let counter = 0 ;
+// // begin the while loop 
+// // figure out the expression that will evaluate to true 
+
+// while(counter <10) {
+//     console.log("gong xi fa cai");
+//     counter += 1;
+// }
+
+// let counter = 0 
+// if (counter < 10 ){
+//     console.log("gong xi fa cai");
+//     counter +=1;
+// }
+
+
+// sam saves $10 every day . Every day he decides  to save $1 more.
+// how many days will it take for sam to save 200 dollars ?
+
+let counter = 0
+
+let days = 1 ;
+let totalSaved = 0;
+let howMuchSaved = 10;
+
+while (totalSaved < 100){
+    totalSaved +=howMuchSaved;
+    howMuchSaved += 1;
+    days += 1;
+
+}
+console.log(days)
