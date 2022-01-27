39f38baa6fbe450aa85976114534d79668b219be
xunne899 <yixun89@live.com>
2022-01-27 03:56:57 +0000

    add

---
+++ 03-branching/terniary.js
@@ -0,0 +1,18 @@
+const 
+
+let x =3
+let y = x++ + 3
+console.log("x after x++ = ", x);
+
+console.log(3+3);
+// <do this if true>
+
+let number = parseInt(prompt("Number please :"))
+if (number < 0 ){
+    number = 0 
+}
+
+number
+
+number % 2 == 0 ? console.log("Even") : console.log("Odd") ;
+console.log(number % 2 == 0 ? "Even" : "Odd");
