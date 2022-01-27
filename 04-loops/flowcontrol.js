c72895a0241f7ec02b7db1dd5fe422358294db4d
xunne899 <yixun89@live.com>
2022-01-27 08:31:51 +0000

    add

---
+++ 04-loops/flowcontrol.js
@@ -0,0 +1,24 @@
+let n = 0 ;
+while (n<10){
+    console.log(n);
+    if(n>5){
+        break;
+    }
+    n++;
+}
+console.log("loop ended");
+
+
+let i = 0
+let j = 0 ;
+while (i<3){
+    while (j<3){
+        if (j == 1){
+            break; 
+        }
+    } 
+       
+    }
+    n++;
+
+console.log("loop ended");
