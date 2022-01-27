7ad1430bf81d4398a627659de83005ed5471d852
xunne899 <yixun89@live.com>
2022-01-27 03:04:27 +0000

    add

---
+++ 03-branching/logical-or.js
@@ -0,0 +1,4 @@
+console.log ("true|| false =>", true || false)
+console.log ("false|| true =>", false || false)
+console.log ("true|| true =>", true || true)
+console.log ("false|| false =>", false || false)
---
+++ 03-branching/logical.js
@@ -0,0 +1,24 @@
+let isRaining = false;
+let isHoliday = true;
+let isWeekend = false;
+
+
+if (isRaining = false && isWeekend == true) {
+console.log ("Go to the beach");
+} else {
+    console.log("Cannot got to the beach");
+
+
+
+    if (isRaining == true){
+        console.log("Bring Umbrella");
+    }
+
+    if (isRaining){
+        console.log("Bring Umbrella");
+    }
+
+    if (!isRaining){
+        console.log ("Go to the beach");
+
+    }
