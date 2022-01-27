364ebba856b1d6f5f741d67b45796a654f97738b
xunne899 <yixun89@live.com>
2022-01-27 03:40:32 +0000

    add

--- 03-branching/truely.js
+++ 03-branching/truely.js
@@ -11,3 +11,10 @@ if ("lamb"){
 if (["a", "b","c"]) {
     console.log("there is an array");
 }
+
+let bill = parsefloat ("please enter amt :")
+
+if (bill){
+let gst = bill *0.07;
+console.log(gst)
+}
