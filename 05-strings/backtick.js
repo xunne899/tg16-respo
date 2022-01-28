d8a8ddd6d50630ba340d37be5c9a1d6ed0504035
xunne899 <yixun89@live.com>
2022-01-28 02:46:20 +0000

    add

--- 05-strings/backtick.js
+++ 05-strings/backtick.js
@@ -2,4 +2,18 @@
 
 let letter = `Dear sir, 
 
-your bill of 500.0 is overdue on 31/ 01 2022
+your bill of 500.0 is overdue on 31/ 01 2022 "customer is always right"`;
+
+console.log(letter)
+
+
+let firstname = "james";
+let lastname = "smith";
+let bill = 200;
+
+let formletter = `
+Dear ${firstname} ${lastname},
+your bill of ${bill}, which will ${bill*0.1}`;
+
+console.log(formletter)
+
