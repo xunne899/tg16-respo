529b8a5edb2a12783cbe72cfc006ae3dc2448ad4
xunne899 <yixun89@live.com>
2022-01-28 02:54:52 +0000

    add

--- 05-strings/backtick.js
+++ 05-strings/backtick.js
@@ -15,5 +15,16 @@ let formletter = `
 Dear ${firstname} ${lastname},
 your bill of ${bill}, which will ${bill*0.1}`;
 
-console.log(formletter)
+console.log(formletter);
+
+let fruits ="bananas" ;
+console.log("fruits[1]", fruits[1])
+
+let number =[1,3,5,7,9];
+numbers[0] =-99
+console.log(numbers);
+
+fruits[0] = 'z'; // strings are immutable, means cannot change individuals in string via index
+
+console.log(fruits);
 
---
+++ 05-strings/strings-function.js
@@ -0,0 +1,4 @@
+let phrase = "the quick brown fox jumps over the lazy dog"
+
+console.log("fourth character", phrase[3])
+console.log("length of the phrase, phase.length");
