b31b1522fb9b0f55c0d20b2fdc17b4b3470e8bd2
xunne899 <yixun89@live.com>
2022-01-28 03:00:48 +0000

    add

--- 05-strings/strings-function.js
+++ 05-strings/strings-function.js
@@ -2,3 +2,14 @@ let phrase = "the quick brown fox jumps over the lazy dog"
 
 console.log("fourth character", phrase[3])
 console.log("length of the phrase, phase.length");
+
+// slice strings
+// start slicing from index 3 all the way to the end
+console.log("phrase.slice(3)", phrase.slice(3));
+console.log ("phrase after slice",phrase)
+
+console.log ("phrase.slice(6,12)", phrase.slice(6,12))
+
+
+console.log ("phrase.slice(6,12)", phrase.slice());
+
