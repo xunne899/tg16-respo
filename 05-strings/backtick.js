5055f57e02573d5375465183d8622a549721ece6
xunne899 <yixun89@live.com>
2022-01-28 02:39:47 +0000

    add

---
+++ 05-strings/backtick.js
@@ -0,0 +1,5 @@
+// back tick string can have line and spacing 
+
+let letter = `Dear sir, 
+
+your bill of 500.0 is overdue on 31/ 01 2022
---
+++ 05-strings/basic-string.js
@@ -0,0 +1,11 @@
+// strings begin with a single quote or double quote
+let words = "quick brown fox jumps over lazy dog"
+let rhyme = "she sells seasells at seashore"
+let paragraph = " dont kill me i dont know anything"
+let story = 'she said, "i sells seashells at the seashore"'
+let story2 = 'she said, "i don\'t know anything"';
+let story3 = "He said, \" gong xi fa cai \"";
+console.log("A\nB")
+console.log("Social\tDistancing");
+console.log("I wish you a merry christmas\n and a happy new year\t for 2022");
+console.log("c:\\documents\\tests");
