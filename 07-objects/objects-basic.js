c343155118ec218496597ec980fd6287e8af6e01
xunne899 <yixun89@live.com>
2022-01-31 02:39:47 +0000

    add

--- 07-objects/objects-basic.js
+++ 07-objects/objects-basic.js
@@ -13,3 +13,44 @@ console.log((fruits['c']));
 // use dot notation 
 // a is not string
 console.log(fruits.a)
+
+// valid variable name in javascript
+
+let student = {
+    'first name': 'John'
+}
+console.log(student['first name'])
+// console.log first name // space is not valid  ... use [] can have space inside
+
+
+let recipe = {
+    title : "spicy Fried Chicken",
+    calories:800,
+    "number of servings" : 3,
+}
+
+// diff from python dict, objs in js can store functions
+
+let spaceship = {
+    speed: 10,
+    calDistTrav: function(time){
+        console.log("attempting to cal distance trav")
+       return this.speed *time;
+    }
+}
+
+let starDestroyer = {
+    speed: 25,
+    calDistTrav: function(time){
+        console.log("attempting to cal distance trav")
+        return this.speed *time**1.5;
+    }
+}
+console.log(spaceship.calDistTrav(10));
+console.log(starDestroyer.calDistTrav(10));
+
+
+
+
+
+
