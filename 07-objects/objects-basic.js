00151db4d947e8e4927421ca1ce21ae8cac6cd58
xunne899 <yixun89@live.com>
2022-01-31 02:19:23 +0000

    add

---
+++ 07-objects/objects-basic.js
@@ -0,0 +1,15 @@
+// objects store key/values pairs , just like in python 
+
+let fruits = {
+ // curly braces means obk=jects
+    'a' :'apples',
+    'b' :'bananas',
+    'c' :'cheeries',
+    
+}
+console.log((fruits['b']));
+console.log((fruits['c']));
+
+// use dot notation 
+// a is not string
+console.log(fruits.a)
